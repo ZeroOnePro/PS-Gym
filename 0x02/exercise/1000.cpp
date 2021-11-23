f3ad780c148138ca0cef6e39a48ba7060e5de413
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-11-24 00:58:23 +0900

    apply ignore binary excutable files

---
+++ 0x02/exercise/1000.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << '\n';
+  return 0;
+}
