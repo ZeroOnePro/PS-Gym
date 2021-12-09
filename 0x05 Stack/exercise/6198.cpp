8600771403b2fc2f7008e124ea1cbcf8ee8bf49e
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-12-10 02:45:17 +0900

    add problem 6198 and solving process

--- 0x05 Stack/exercise/6198.cpp
+++ 0x05 Stack/exercise/6198.cpp
@@ -6,6 +6,19 @@ int main(void)
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  cout << "heloow";
+  int n;
+  cin >> n;
+  stack<long long> building;
+  long long total = 0;
+  while (n--)
+  {
+    long long height;
+    cin >> height;
+    while (!building.empty() && building.top() <= height)
+      building.pop();
+    total += building.size();
+    building.push(height);
+  }
+  cout << total;
   return 0;
 }
