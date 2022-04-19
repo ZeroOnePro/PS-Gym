093b27250e92c721277e984bb5a2157d0a679aec
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-04-20 04:09:58 +0900

    add 2146, 2146 optimization solution - bfs

--- 0x09 BFS/exercise/2146_Optimization.cpp
+++ 0x09 BFS/exercise/2146_Optimization.cpp
@@ -1,3 +1,8 @@
+// 여러 시작점이 있는 경우의 bfs풀이를 응용한 다리 찾기 문제
+// 참고링크:
+// http://www.secmem.org/blog/2019/03/07/%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EB%8C%80%EB%B9%84-%ED%8A%B9%EA%B0%95/
+// 59 슬라이드
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -29,22 +34,26 @@ void count_area(queue<pair<int, int>>& q, int area_num) {
   }
 }
 
-int construct_bridge(queue<pair<int, int>>& q, int area_num) {
+int construct_bridge(queue<pair<int, int>>& q, int* min_len) {
   while (!q.empty()) {
     auto cur = q.front();
     q.pop();
     for (int dir = 0; dir < 4; ++dir) {
       int nx = cur.x + dx[dir];
       int ny = cur.y + dy[dir];
-      if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+      if (nx < 0 or nx >= n or ny < 0 or ny >= n) continue;
       // 같은 구역이면, 더 볼 필요없다.
-      if (dist[nx][ny] >= 0 || country[nx][ny] == area_num) continue;
+      if (country[nx][ny] == country[cur.x][cur.y]) continue;
       // 다른 구역을 만났을 때
-      if (country[nx][ny] != 0 && country[nx][ny] != area_num) {
-        return dist[cur.x][cur.y];
+      if (country[nx][ny] != 0) {
+        *min_len = min(*min_len, dist[cur.x][cur.y] + dist[nx][ny]);
+      } else {
+        q.push({nx, ny});
+        country[nx][ny] =
+            country[cur.x][cur.y];  // 바다를 섬으로 채운다, dist배열을 초기화할
+                                    // 필요가 없어진다.
+        dist[nx][ny] = dist[cur.x][cur.y] + 1;
       }
-      q.push({nx, ny});
-      dist[nx][ny] = dist[cur.x][cur.y] + 1;
     }
   }
   return INT_MAX;
@@ -76,19 +85,18 @@ int main(void) {
   }
   while (!q.empty()) q.pop();
   for (int i = 0; i < n; ++i) fill(dist[i], dist[i] + n, -1);
-  int min_bridge_len = INT_MAX;
+
+  // 모든 점 하나하나 할 필요없이 섬을 단위(시작점)로 동시에 bfs를 수행한다.
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
       if (country[i][j] >= 1) {
         q.push({i, j});
         dist[i][j] = 0;
-        min_bridge_len =
-            min(min_bridge_len, construct_bridge(q, country[i][j]));
       }
-      while (!q.empty()) q.pop();
-      for (int k = 0; k < n; ++k) fill(dist[k], dist[k] + n, -1);
     }
   }
+  int min_bridge_len = INT_MAX;
+  construct_bridge(q, &min_bridge_len);
   cout << min_bridge_len << '\n';
   return 0;
 }
