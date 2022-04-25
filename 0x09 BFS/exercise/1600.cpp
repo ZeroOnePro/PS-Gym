a7e3c2c39a548b5b681c28ffc646d5195fc3bdad
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-04-26 04:00:16 +0900

    add 1600 template - bfs

---
+++ 0x09 BFS/exercise/1600.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 벽 부수고 이동하기(2206)에서 벽을 여러번 부술 수 있는 버전이네
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}
