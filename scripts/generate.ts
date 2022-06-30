ebdbfd19464341cdb8c2be2afc030abeddaf475f
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-06-30 12:32:45 +0900

    [#45] change api call divide chunk

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,10 +1,23 @@
+import { lodash } from "./index";
 import { getFiles, getProblems, processFile } from "./data";
 import { generateMarkdown } from "./markdown";
+import { ProblemData } from "./type";
 
 const generate = async () => {
   const cppFiles = await getFiles();
   const [problemNums, categories] = processFile(cppFiles);
-  const problems = await getProblems(problemNums);
+  const problems: ProblemData[] = [];
+  if (problemNums.length > 100) {
+    const digit = Math.ceil(problemNums.length / 100);
+    console.log(lodash.uniq(problemNums).length);
+    for (let iter = 1; iter <= digit; ++iter) {
+      problems.push(
+        ...(await getProblems(problemNums.slice(100 * (iter - 1), 100 * iter)))
+      );
+    }
+  } else {
+    problems.push(...(await getProblems(problemNums)));
+  }
   generateMarkdown(problems, categories);
 };
 
