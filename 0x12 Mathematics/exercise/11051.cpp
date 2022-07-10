90a4d27616a0a0670f1a2c47d51947d5c6279717
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-07-11 02:36:59 +0900

    [#21] add meterial

--- 0x12 Mathematics/exercise/11051.cpp
+++ 0x12 Mathematics/exercise/11051.cpp
@@ -9,7 +9,7 @@ int main(void) {
   for (int i = 1; i <= 1000; ++i) {
     dp[i][0] = dp[i][i] = 1;
     for (int j = 1; j < i; ++j) {
-      dp[i][j] = (dp[i - 1][k] + dp[i - 1][k - 1]) % 10007;
+      dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % 10007;
     }
   }
   int n, m;
