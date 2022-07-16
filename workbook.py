76bbe4c58a92e67a91df9beec82c5175cf053455
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-07-16 09:03:32 +0900

    [#46] add script to scrapping workbook problems

---
+++ workbook.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+
+chapters = ["0x{:02X}".format(i) for i in range(2, 31)]
+
+workbook = {}
+
+for chapter in chapters:
+  url = f'https://github.com/encrypted-def/basic-algo-lecture/blob/master/workbook/{chapter}.md'
+  try:
+    table = pd.read_html(url)
+    problems = [ problem for problem in table[0]["문제"]]
+    workbook[f"{chapter}"] = problems
+  except Exception as e:
+    print('예외 발생 사유: ', e)
+
+with open('./workbook.json', 'w', encoding='utf-8') as file:
+      json.dump(workbook, file)
