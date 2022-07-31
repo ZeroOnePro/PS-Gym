5a7c91be2dc7dc76f71f15c330797a6f5d4ffddf
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-08-01 00:39:26 +0900

    [#46] modify toc scripts flexible

--- scripts/insert.toc.sh
+++ scripts/insert.toc.sh
@@ -1,1 +1,5 @@
-echo '<!--ts-->\n<!--te-->\n' | sed -i '' '2r/dev/stdin' "$INIT_CWD/README.md" && ~/gh-md-toc --insert --no-backup "$INIT_CWD/README.md"
+read -p "목차를 삽입할 파일 절대 경로를 붙여넣어 주세요 >> " current
+
+# use gnu-sed on macOS
+
+sed -i "2i <!--ts-->\n<!--te-->\n" "$current" && ~/gh-md-toc --insert --no-backup "$current"
--- scripts/update.toc.sh
+++ scripts/update.toc.sh
@@ -1,1 +1,3 @@
-~/gh-md-toc --insert "$INIT_CWD/README.md"
+read -p "목차를 수정할 파일 절대 경로를 붙여넣어 주세요 >> " current
+
+~/gh-md-toc --insert "$current"
