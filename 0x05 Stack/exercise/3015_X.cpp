ff2ebd44ef347e320a73c4e6dfa64e9f7d6f1fb5
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-08-08 15:20:32 +0900

    [#44] add 0x05, 3015

--- 0x05 Stack/exercise/3015_X.cpp
+++ 0x05 Stack/exercise/3015_X.cpp
@@ -2,27 +2,23 @@
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int n;
   cin >> n;
   stack<pair<int, int>> s;
   long long ans = 0;
-  while (n--)
-  {
+  while (n--) {
     int height, cnt = 1;
     cin >> height;
-    while (!s.empty() && s.top().first <= height)
-    {
-      if (s.top().first == height) // 서로 같은 높이가 들어왔을 때를 고려하지 않았다..
-        cnt += s.top().second;
+    while (!s.empty() && s.top().first <= height) {
+      // 서로 같은 높이가 들어왔을 때를 고려하지 않았다..
+      if (s.top().first == height) cnt += s.top().second;
       ans += s.top().second;
       s.pop();
     }
-    if (!s.empty() && s.top().first > height)
-      ans++;
+    if (!s.empty() && s.top().first > height) ans++;
     s.push({height, cnt});
   }
   cout << ans;
