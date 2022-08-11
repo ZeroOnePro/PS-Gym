4bb737f07e0d6ac1616ca47cfd4541d23715662e
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-08-12 01:56:12 +0900

    [#44] add 0x08 keynote and refactor exercise problems

--- 0x08 Incorrect paif of parentheses/exercise/3986.cpp
+++ 0x08 Incorrect paif of parentheses/exercise/3986.cpp
@@ -2,35 +2,30 @@
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int goodWord = 0;
+  int good_word = 0;
   // top과 다르면 pop x
   // top과 같으면 없애버리자
   int tc;
   cin >> tc;
-  while (tc--)
-  {
+  while (tc--) {
     string word;
     cin >> word;
     stack<char> s;
-    for (char ch : word)
-    {
+    for (char ch : word) {
       if (s.empty())
         s.push(ch);
-      else
-      {
+      else {
         if (s.top() == ch)
           s.pop();
         else
           s.push(ch);
       }
     }
-    if (s.empty())
-      goodWord += 1;
+    if (s.empty()) good_word += 1;
   }
-  cout << goodWord << '\n';
+  cout << good_word << '\n';
   return 0;
 }
