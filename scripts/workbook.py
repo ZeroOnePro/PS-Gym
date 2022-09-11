caaa24a7522724d4acc1c4b337e65190df431c8a
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-09-11 22:19:21 +0900

    [#46] modify workbook script

--- scripts/workbook.py
+++ scripts/workbook.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import json
-import multiprocessing as mp
-import time
 
 chapters = ["0x{:02X}".format(i) for i in range(2, 32)]
 
 workbook = {}
 
-def get_problems(chapter):
+for chapter in chapters:
   url = f'https://github.com/encrypted-def/basic-algo-lecture/blob/master/workbook/{chapter}.md'
   try:
     table = pd.read_html(url)
@@ -16,11 +14,5 @@ def get_problems(chapter):
   except Exception as e:
     print('예외 발생 사유: ', e)
 
-if __name__=='__main__':
-  start_time = time.time()
-  pool = mp.Pool(processes=mp.cpu_count())
-  pool.map(get_problems, chapters)
-  print("--- %s seconds ---" % (time.time() - start_time))
-
-  with open('workbook.json', 'w', encoding='utf-8') as file:
-        json.dump(workbook, file)
+with open('./scripts/workbook.json', 'w', encoding='utf-8') as file:
+      json.dump(workbook, file)
