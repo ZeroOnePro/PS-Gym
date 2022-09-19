f5eec2bd8266268d6bbd3bd18520f211674f1527
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-09-19 21:54:10 +0900

    move 15688 0x0F -> 0x0E

--- 0x0E Sorting I/exercise/15688.cpp
+++ 0x0E Sorting I/exercise/15688.cpp
@@ -2,14 +2,33 @@
 
 using namespace std;
 
+#define RANGE 1000000
+
+int n;
+int arr[RANGE + 2];
+int freq[RANGE * 2 + 2];
+
+void counting_sort(void) {
+  int st = RANGE + 1, en = RANGE * -1 - 1;
+  for (int i = 0; i < n; ++i) {
+    st = min(arr[i], st);
+    en = max(arr[i], en);
+    freq[arr[i] + RANGE] += 1;
+  }
+  for (int i = st; i <= en; ++i) {
+    for (int j = 0; j < freq[i + RANGE]; ++j) {
+      cout << i << '\n';
+    }
+  }
+}
+
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int n;
   cin >> n;
-  vector<int> v(n);
-  for (int i = 0; i < n; ++i) cin >> v[i];
-  sort(v.begin(), v.end());
-  for (int i : v) cout << i << '\n';
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  counting_sort();
   return 0;
 }
