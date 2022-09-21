eba64030df82dd3224ce2a0cf99c7252d09bcd4f
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-09-21 17:33:19 +0900

    [#44] add 0x10 keynote - until 12852

--- 0x10 Dynamic Programming/exercise/11726.cpp
+++ 0x10 Dynamic Programming/exercise/11726.cpp
@@ -10,10 +10,9 @@ int main(void) {
   int dp[n - 1];
   dp[1] = 1;
   dp[2] = 2;
-  for (int i = 3; i <= n; ++i) {
-    dp[i] =
-        (dp[i - 1] + dp[i - 2]) % 10007;  // (a + b) mod n = a mod n + b mod n
-  }
+  for (int i = 3; i <= n; ++i)
+    // (a + b) mod n = a mod n + b mod n
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
   cout << dp[n] << '\n';
   return 0;
 }
