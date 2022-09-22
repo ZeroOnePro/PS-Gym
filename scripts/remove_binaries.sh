a02a37b7aff09a4be610d0299acc480d5f521282
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-09-22 13:16:13 +0900

    [#46] delete duplicate file and rename clear script

--- scripts/remove_binaries.sh
+++
@@ -1,3 +0,0 @@
-find . -type f ! \( -path './.git*' -prune -o -path "./node_modules*" -prune -o -path "./github*" -prune -o -path "./.vscode*" -prune \) -not -name "*.*" -exec rm {} \;
-
-echo "모든 binary 실행파일을 삭제했습니다."
