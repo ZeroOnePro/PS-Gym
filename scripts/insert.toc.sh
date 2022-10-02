e1a089ad8411b13255bd1e90edb948bd2e5950de
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-10-03 02:02:55 +0900

    [#46] fix confilct gitignore execuatable file

--- scripts/insert.toc.sh
+++ scripts/insert.toc.sh
@@ -4,4 +4,4 @@ read -p "목차를 삽입할 파일 절대 경로를 붙여넣어 주세요 >> "
 
 echo $current
 
-sed -i "2i <!--ts-->\n<!--te-->\n" "$current" && ./scripts/gh-md-toc --insert --no-backup "$current"
+sed -i "2i <!--ts-->\n<!--te-->\n" "$current" && ./scripts/gh-md-toc.sh --insert --no-backup "$current"
