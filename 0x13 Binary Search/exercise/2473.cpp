317de8ae9062ff409da0b2ec55c237854f5bf847
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-10-25 19:02:50 +0900

    [#44] 0x13 kenote - until 2110

--- 0x13 Binary Search/exercise/2473.cpp
+++ 0x13 Binary Search/exercise/2473.cpp
@@ -2,19 +2,21 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int n;
-  vector<int> ans;
-  long long mn = 3 * 1e9 + 5;
+  vector<ll> ans;
+  ll mn = 3 * 1e9 + 5;
   cin >> n;
-  vector<int> sol(n);
+  vector<ll> sol(n);
   for (auto& pt : sol) cin >> pt;
   sort(sol.begin(), sol.end());
   for (int i = 0; i < n - 1; ++i)
     for (int j = i + 1; j < n; ++j) {
-      int target = -sol[i] - sol[j];
+      ll target = -sol[i] - sol[j];
       int lb = lower_bound(sol.begin(), sol.end(), target) - sol.begin();
       for (int cur = -2; cur <= 2; ++cur) {
         int idx = lb + cur;
@@ -27,6 +29,6 @@ int main(void) {
       }
     }
   sort(ans.begin(), ans.end());
-  for (int s : ans) cout << s << ' ';
+  for (ll s : ans) cout << s << ' ';
   return 0;
 }
