dc0c109a1bf349926d45eb2f6c92cc26b39085d4
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-11-01 13:26:51 +0900

    [#23] modify 2283

--- 0x14 Two Pointer/exercise/2283_X.cpp
+++ 0x14 Two Pointer/exercise/2283_X.cpp
@@ -18,8 +18,7 @@ int main(void) {
     len[a + 1] += 1;
     len[b + 1] -= 1;
   }
-  // 점 a에서 제외되는 선의 개수 a-1까지 선들의 길이의 합 + a ~ a-1에 포함된
-  // 선의 개수
+  // 커서가 a를 가리킬 때 [0, a]에 포함되는 선 길이의 총합
   for (int i = 1; i < range; ++i) len[i] += len[i - 1];
   int st = 0, en = 0, sum = 0;
   while (en < range) {
