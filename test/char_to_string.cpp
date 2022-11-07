f0ca11cbde5bdbfb8c80c62e7ad77f18358541b0
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-11-07 14:05:14 +0900

    [#44] 0x15 keynote finish, close #24

---
+++ test/char_to_string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  char ch = 'a';
+  string s = "" + ch;
+  cout << s;
+  return 0;
+}
