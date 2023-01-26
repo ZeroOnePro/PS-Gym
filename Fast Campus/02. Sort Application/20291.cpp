180b83936ed782a42e9aa5cdb887f1019137dcf3
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2023-01-26 16:21:08 +0900

    [#49] add sorting - boj 20291

---
+++ Fast Campus/02. Sort Application/20291.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  // . 앞에 문자열 날리고 이후 확장자만 저장해서 정렬해서 몇 개 있는지 세기
+  // 확실히 hash쓰면 매우 간편하긴 하겠다
+  vector<string> file;
+  for (int i = 0; i < n; ++i) {
+    string file_name;
+    cin >> file_name;
+  }
+  return 0;
+}
