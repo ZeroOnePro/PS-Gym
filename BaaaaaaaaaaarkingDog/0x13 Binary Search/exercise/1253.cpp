70c9497c938aedb6dfaff6f18e84c660c85d56e6
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2023-02-14 01:19:12 +0900

    [#44] refactor 1253

--- BaaaaaaaaaaarkingDog/0x13 Binary Search/exercise/1253.cpp
+++ BaaaaaaaaaaarkingDog/0x13 Binary Search/exercise/1253.cpp
@@ -2,35 +2,44 @@
 
 using namespace std;
 
+int n, ans;
+vector<int> a;
+
+bool decision(int idx) {
+  int l = 0, r = n - 1;
+  int target = a[idx];
+  while (l < r) {
+    if (l == idx)
+      l++;
+    else if (r == idx)
+      r--;
+    else {
+      // 최소 + 최대 < 0, 최소의 입장에선 최대 원소 제외 다른 임의의 원소와
+      // 합을 해도 더 작은 음수가 된다 최소는 쌍을 찾은 것
+      if (a[l] + a[r] < target) l++;
+      // 최소 + 최대 > 0, 최대의 입장에선 최소 원소 제외 다른 임의의 원소와
+      // 합을 해도 더 큰 양수가 된다 최대는 쌍을 찾은 것
+      else if (a[l] + a[r] > target)
+        r--;
+      else
+        return true;
+    }
+  }
+  return false;
+}
+
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int n, ans = 0;
   cin >> n;
-  vector<int> a(n);
-  vector<bool> is_used(n);
-  int cnt = 0;
-  for (auto& pt : a) {
-    cin >> pt;
-    cnt += pt;
-  }
-  if (cnt == 0) {
-    cout << n;
-    return 0;
+  for (int i = 0; i < n; ++i) {
+    int e;
+    cin >> e;
+    a.push_back(e);
   }
   sort(a.begin(), a.end());
-  for (int i = 0; i < n - 1; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      int target = a[i] + a[j];
-      int ub = upper_bound(a.begin(), a.end(), target) - a.begin();
-      int lb = lower_bound(a.begin(), a.end(), target) - a.begin();
-      for (int cur = lb; cur < ub; ++cur)
-        if (!is_used[cur] && cur != i && cur != j) {
-          ans += 1;
-          is_used[cur] = true;
-        }
-    }
-  }
+  for (int i = 0; i < n; ++i)
+    if (decision(i)) ans += 1;
   cout << ans;
   return 0;
 }
