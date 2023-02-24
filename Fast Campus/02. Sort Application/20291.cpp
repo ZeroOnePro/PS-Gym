d4eb940235c7aa55f062d8a22a4a338b893f47ff
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2023-02-24 17:37:13 +0900

    [#49] chapter 2 - add 20291

--- Fast Campus/02. Sort Application/20291.cpp
+++ Fast Campus/02. Sort Application/20291.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+vector<string> parse(string origin, string delimiter) {
+  vector<string> token;
+  size_t pos = 0;
+  while ((pos = origin.find(delimiter)) != string::npos) {
+    token.push_back(origin.substr(0, pos));
+    origin.erase(0, pos + delimiter.length());
+  }
+  token.push_back(origin);
+  return token;
+}
+
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -13,6 +24,21 @@ int main(void) {
   for (int i = 0; i < n; ++i) {
     string file_name;
     cin >> file_name;
+    size_t pos = file_name.find('.');
+    file.push_back(file_name.substr(pos + 1));
+  }
+  sort(file.begin(), file.end());
+  string cur = file[0];
+  int cnt = 0;
+  file.push_back("proxy");
+  for (int i = 0; i < file.size(); ++i) {
+    if (cur == file[i])
+      cnt += 1;
+    else {
+      cout << file[i - 1] << ' ' << cnt << '\n';
+      cur = file[i];
+      cnt = 1;
+    }
   }
   return 0;
 }
